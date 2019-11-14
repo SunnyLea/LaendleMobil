7ce2d829c2844896a4517373956bc37c4363a2df
= <=>
2019-11-14 21:20:30 +0100

    Eingabeprüfung Select

--- functions.js
+++ functions.js
@@ -1,32 +1,57 @@
-function showTable() {
-    let foundDrive = document.getElementById("foundDrive");
-    foundDrive.style.display='block'; 
-}
-
 function start(){
     let foundDrive = document.getElementById("foundDrive");
     foundDrive.style.display='none'; 
 }
 
-function checkEntries(){
-   /* for(var i=0; i<)*/
+start();
+
+
+function checkEntriesStart(){
    let inputs = document.getElementsByTagName('input');
    let noInput = false;
-   for(let i = 0; i<inputs.length; i++){
+
+   for(let i = 0; i < inputs.length; i++){
        if(inputs[i].value == ""){
            noInput = true;
        }
-       else{
-           showTable();
-       }
    }
-   let select = document.getElementsByTagName('select');
-   if(select.value === "Bitte auswählen ..."){
+   let select = document.getElementById('seekedTime');
+   if(select.value == "Bitte auswählen ..."){
        noInput = true;
    }
-   if(noInput){
+    if(!noInput){
+       showTable();
+    }
+    if(noInput){
         alert("Bitte Daten eingeben");
    }
 }
 
-start();
+function showTable() {
+    let foundDrive = document.getElementById("foundDrive");
+    foundDrive.style.display='block'; 
+}
+
+/*
+function checkEntriesFA(){
+   let inputs = document.getElementsByTagName('input');
+   let noInput = false;
+
+   for(let i = 0; i < inputs.length; i++){
+       if(inputs[i].value == ""){
+           noInput = true;
+       }
+   }
+   let select = document.getElementById('selectFA');
+   if(select.value == "Bitte auswählen ..."){
+       noInput = true;
+   }
+    if(!noInput){
+       
+    }
+    if(noInput){
+        alert("Bitte Daten eingeben");
+   }
+}
+*/
+
