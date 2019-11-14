13a8cc983660d3cc106266b7312bbc0a89033b9f
=puschnadine <=>
2019-11-14 11:24:57 +0100

    Java Script

---
+++ functions.js
@@ -0,0 +1,15 @@
+function showTable() {
+    let foundDrive = document.getElementById("foundDrive");
+    foundDrive.style.display='block'; 
+}
+
+function start(){
+    let foundDrive = document.getElementById("foundDrive");
+    foundDrive.style.display='none'; 
+}
+
+function checkEntries(){
+   /* for(var i=0; i<)*/
+}
+
+start();
