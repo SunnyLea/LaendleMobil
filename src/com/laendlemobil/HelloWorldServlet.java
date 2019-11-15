4ecde9d8ca9fa6f365c475f0b7159b4f56a680e4
=puschnadine <=>
2019-11-15 15:30:22 +0100

    zusätzliche Felder

--- src/com/laendlemobil/HelloWorldServlet.java
+++ src/com/laendlemobil/HelloWorldServlet.java
@@ -1,6 +1,8 @@
 package com.laendlemobil;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,8 +13,26 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorldServlet extends HttpServlet {
 	
 	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String message = req.getParameter("message");
 		System.out.println(req.getParameter("message"));
+		writeToDatabase(message);
+	}
+	
+	private void writeToDatabase(String message) {
+		try {
+			Connection conn = DriveManager.getConnection("jdbc:mysql://localhost:3306/messages?useSSL=false", "root", "laendlemob1L")			
+					Statement statement = conn.createStatement();
+			
+			String insert = "insert into messages(message) value ('" + message + "')";
+			
+			statement.executeUpdate(insert);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		
 	}
 
 	
